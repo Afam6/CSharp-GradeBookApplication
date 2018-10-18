460d4ac739c054acf4d17c1bd42fad1c5cdddb78
Afam <afamezechukwu@gmail.com>
2018-10-18 14:53:49 +0100

    Forgot semi-colon

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,11 +1,11 @@
-﻿using GradeBook.Enums
+﻿using GradeBook.Enums;
 namespace GradeBook.GradeBooks
 {
     public class RankedGradeBook : BaseGradeBook
     {
         public RankedGradeBook(string name) : base(name)
         {
-            Type = GradeBookType.Ranked
+            Type = GradeBookType.Ranked;
         }
     }
 }
